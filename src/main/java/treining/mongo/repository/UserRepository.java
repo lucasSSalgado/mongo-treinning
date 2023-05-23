f7638e838c095ff0d03c51c71b2873dae2851dc9
lucasSSalgado <lucssslucsss@gmail.com>
2023-05-23 12:06:26 -0300

    creation of a population class

--- src/main/java/treining/mongo/repository/UserRepository.java
+++ src/main/java/treining/mongo/repository/UserRepository.java
@@ -5,5 +5,5 @@ import org.springframework.stereotype.Repository;
 import treining.mongo.domain.User;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, Long> {
+public interface UserRepository extends MongoRepository<User, String> {
 }
